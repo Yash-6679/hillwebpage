d21d92d962edb0b4584ae0f27f6a6b1dc3f7bb1e
Yash <ndyash962005@gmail.com>
2026-08-17 02:36:22 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -546,7 +546,7 @@ function App() {
                 <div className="bg-[#0D0907] p-4 rounded-2xl shadow-2xl border border-[#8B4513]/20">
                   <div className="aspect-video rounded-xl overflow-hidden">
                     <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3914.7234567890123!2d76.8654321!3d11.4123456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sMettupalayam%20Road%2C%20Kotagiri%2C%20Tamil%20Nadu%20643217!5e0!3m2!1sen!2sin!4v1234567890123!5m2!1sen!2sin"
+                      src="https://maps.app.goo.gl/FcEBrD5NLv2C8o6m8"
                       width="100%"
                       height="100%"
                       style={{ border: 0 }}
